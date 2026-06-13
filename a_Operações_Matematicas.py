9440d7c0c45b1dbea6fa5045defec7f0a1e3bf0e
Pedro H Southier <eng.pedrohsouthier@gmail.com>
2026-06-12 22:55:58 -0300

    Tentando salvar final da aula

---
+++ a_Operações_Matematicas.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+n = int(input("n: "))
+
+dobro = n * 2
+triplo = n * 3
+quadrado = n ** 2
+cubo = n ** 3
+raizQuadrada = sqrt(n)2
+
+print()
+print(f"Sendo n = {n}, tem-se: ") 
+print(f"Drobro = {dobro} ") 
+print(f"Triplo = {triplo} ") 
+print(f"Quadrado = {quadrado} ") 
+print(f"Cubo = {cubo} ") 
+print(f"Raiz Quadrada = {raizQuadrada:.4f}")
